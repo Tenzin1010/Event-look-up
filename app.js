22a9271eb2d7281fffb33784db008535e8e13e9b
Tenzin Ngawang <tenzin.ngawang0@gmail.com>
2019-06-29 01:05:31 -0400

    Updated files with styling

--- app.js
+++ app.js
@@ -2,63 +2,106 @@
 const myApiKey = "w7M56mXGA96et4g2WyUYD5l7AJIBVdYF";
 const baseUrl = "https://app.ticketmaster.com/discovery/v2/events/";
 
-
-function formatQueryPara(params){
-    const queryItems = Object.keys(params)
+//gets paramaters from getEvents func & transforms the key & value (from user) to a string format & sent back to getEvents
+function formatQueryPara(params) {
+  const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
+//func displays positive/negative msgs. Append
 function displayResults(responseJson) {
   const eventResults = $('#event-results');
-  eventResults.removeClass('hidden');
+  const eventNoResults = $('#event-noresults');
+
+  // clears the result div, error message if there is one present
   eventResults.empty();
   $('#js-error-message').empty();
-  
+
+  // returned response is not Json or response is Json but it doesn't contain object _embedded
+  //remove any type of result page if any 1st before displaying response
   const noResults = !responseJson || (responseJson && !responseJson._embedded);
   if (noResults) {
-    eventResults.html('No Results');
+    eventResults.addClass('hidden');
+    eventNoResults.removeClass('noresult-hidden');
+    eventNoResults.html('No results found');
+    resetButton();
     return;
   }
-  const {events = []}  = responseJson._embedded;
+  //if the response has no events, display No result
+  //remove any type of result page if any 1st before displaying response
+  const { events = [] } = responseJson._embedded;
   if (!events.length) {
-    eventResults.html('No Results');
+    eventResults.addClass('hidden');
+    eventNoResults.removeClass('noresult-hidden');
+    eventNoResults.html('No results found');
+    resetButton();
     return;
   }
+  $('#event-noresults').addClass('noresult-hidden');
+  eventResults.removeClass('hidden');
 
+  //displays response
   const embeddedEvents = responseJson._embedded.events;
-  for (let i=0; i<responseJson._embedded.events.length; i++) {
+  for (let i = 0; i < responseJson._embedded.events.length; i++) {
     eventResults.append(`
-      <ol>
-        <li><h3>${embeddedEvents[i].name}</h3>
-            <img src="${embeddedEvents[i].images[0].url}">
-            <p><a href="${embeddedEvents[i].url}" target="_blank">Purchase event ticket</a></p>
-            <p>${embeddedEvents[i]._embedded.venues[0].address.line1}</P>
-            <p>${embeddedEvents[i]._embedded.venues[0].city.name}</p>
-            <p>${embeddedEvents[i].dates.start.localDate}</p>
-            <p>${embeddedEvents[i].dates.start.localTime}</p>
-        </li>
-      </ol>`
-    )};
-  // alternative 
-  //const eventNames = events.map(x => x.name).join();
-  // eventResults.html(eventNames);
+    
+    
+        <li class="search-results-li">
+            <div class="float-item-details">
+                <div class="event-item-date-image">
+                    <span>${$.format.date(new Date(embeddedEvents[i].dates.start.localDate), "E MMM d")}</span>
+                    <img
+                        src="${embeddedEvents[i].images[0].url}">
+                </div>
+
+                <div class="item-details">
+                    <div class="item-header">
+                        ${embeddedEvents[i].name}
+                    </div>
+
+                    <div class="item-MixInfo">
+                        <span>
+                            <time>
+                                <span>${embeddedEvents[i].dates.start.localTime}</span>
+                            </time>
+                            -
+                            <span class="item-venue-name">
+                                ${embeddedEvents[i]._embedded.venues[0].name}
+                                ,
+                                ${embeddedEvents[i]._embedded.venues[0].address.line1},
+                                ${embeddedEvents[i]._embedded.venues[0].city.name}
+                            </span>
+                        </span>
+
+                    </div>
+                    <div class="item-purchase">
+                        <a href="${embeddedEvents[i].url}" target="_blank">Purchase event ticket</a>
+                    </div>
+                </div>
+            </div>
+        </li>`
+
+    )
+  };
+  resetButton();
 }
 
+//func 
+function getEvents(userSearchTerm, userSearchTermCity, maxResults = 10) {
+  const params = {
+    classificationName: userSearchTerm,
+    city: userSearchTermCity,
+    size: maxResults,
+    apikey: myApiKey
+  };
+  //func formatQueryPara is called, the params are returned as a URL string which is added to website url + endpoint
+  //which will be used to fetch from the API
+  const queryString = formatQueryPara(params);
+  const appUrl = baseUrl + '?' + queryString;
+  console.log(appUrl);
 
-function getEvents(userSearchTerm, userSearchTermCity, maxResults=10){
-  
-    const params = {
-        classificationName: userSearchTerm,
-        city: userSearchTermCity,
-        size: maxResults,
-        apikey: myApiKey
-    };
-    const queryString = formatQueryPara(params);
-    const appUrl = baseUrl + '?' + queryString;
-    console.log(appUrl);
-
-    fetch(appUrl)
+  fetch(appUrl)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -67,23 +110,34 @@ function getEvents(userSearchTerm, userSearchTermCity, maxResults=10){
     })
     .then(displayResults)
     .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#event-results').addClass('hidden');
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      resetButton();
     });
 
 }
+//func creates the reset btn action
+function resetButton() {
+  $('#reset-btn').on('click', function () {
+    $('#event-results').addClass('hidden');
+    $('#event-noresults').addClass('noresult-hidden');
+    $('#js-error-message').empty();
 
+  });
+}
+//func captures 3 differnt input from user and sends to getEvents func
 function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        const searchTerm = $('#js-search-term').val();
-        const searchTermCity = $('#js-search-term-city').val();
-        const maxResults = $('#js-max-results').val();
-                 
-        getEvents(searchTerm, searchTermCity, maxResults);
-    });  
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const searchTermCity = $('#js-search-term-city').val();
+    const maxResults = $('#js-max-results').val();
+
+    getEvents(searchTerm, searchTermCity, maxResults);
+  });
 }
 
-$(function() {
-    watchForm();
-    console.log('doc ready');
+$(function () {
+  watchForm();
+  console.log('doc ready');
 })
